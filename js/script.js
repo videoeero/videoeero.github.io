7cd75ce8b63531e68f5e76e0ff5b334b0b230b48
videoeero <eero.suvanto@gmail.com>
2018-01-26 18:25:07 +0200

    Countdown testi

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var options = {
+  "animate": true,
+  "patternWidth": 166.69,
+  "patternHeight": 301.77,
+  "grainOpacity": 0.3,
+  "grainDensity": 1,
+  "grainWidth": 2.19,
+  "grainHeight": 1
+}
+grained("#landing", options);
+
+
