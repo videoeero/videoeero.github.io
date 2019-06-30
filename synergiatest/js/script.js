3d305f226eefdd03daeca2374dd76121c539fadf
videoeero <eero.suvanto@gmail.com>
2019-07-01 00:12:24 +0300

    (maybe) fixed title animation

--- synergiatest/js/script.js
+++ synergiatest/js/script.js
@@ -1,1 +1,1 @@
-let title=document.getElementById("landing-title");function loadParticles(){particlesJS.load("particles-js","particle-cfg.json")}title.innerHTML="Synergia",title.classList.add("opacity"),$(window).on("load",function(){setInterval(function(){"Synergia"===title.innerHTML?title.innerHTML="20.11.19":title.innerHTML="Synergia"},5e3)});
+function loadParticles(){particlesJS.load("particles-js","particle-cfg.json")}
