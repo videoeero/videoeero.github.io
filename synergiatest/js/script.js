c88b5b256900ec6b6112f0c356f42a28003e3cf4
videoeero <eero.suvanto@gmail.com>
2019-07-01 00:30:30 +0300

    scroll to bottom

--- synergiatest/js/script.js
+++ synergiatest/js/script.js
@@ -1,1 +1,4 @@
-function loadParticles(){particlesJS.load("particles-js","particle-cfg.json")}
+function loadParticles() {
+	particlesJS.load('particles-js', 'particle-cfg.json');
+}
+window.scrollTo(0, document.body.scrollHeight);
